4ebee80d55b338ff4dfe34b3cff7c1798e9c29f5
DKB007 <bhattdevarsh1.db@gmail.com>
2021-04-02 19:20:04 +0530

    Initial Commit

--- app/src/main/java/com/app/loginscreen01/MainActivity.java
+++ app/src/main/java/com/app/loginscreen01/MainActivity.java
@@ -2,13 +2,27 @@ package com.app.loginscreen01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signup = findViewById(R.id.Signup);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Registration.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/app/loginscreen01/Registration.java
+++ app/src/main/java/com/app/loginscreen01/Registration.java
@@ -10,5 +10,7 @@ public class Registration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registration_main);
+
+
     }
 }
